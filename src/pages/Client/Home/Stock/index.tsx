f85baf946ca51a16ed34ac00f1204a7cfef84cdc
Lauro <lauropimentel12@gmail.com>
2024-08-14 19:25:31 -0300

    logout e botao de membro

--- src/pages/Client/Home/Stock/index.tsx
+++ src/pages/Client/Home/Stock/index.tsx
@@ -28,7 +28,7 @@ function Stock() {
 
     return (
         <section id="stock" className="flex flex-col items-center pb-16">
-            <h3 className="pb-5 text-center text-xl font-bold uppercase">Procurar por modelo</h3>
+            <h3 className="pb-5 text-center text-xl font-bold uppercase">Estoque</h3>
             <div className="container mx-auto py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {vehicles !== null ? (
                     vehicles.map((vehicle) => (
